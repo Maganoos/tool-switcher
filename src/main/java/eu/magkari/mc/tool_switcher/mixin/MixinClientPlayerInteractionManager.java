d80e37a21fb17c40dbbc607d45bbced5802786b7
Magnus <magkari@pm.me>
2025-08-22 18:40:02 +0000

    idk what I did I forgor

--- src/main/java/eu/magkari/mc/tool_switcher/mixin/MixinClientPlayerInteractionManager.java
+++ src/main/java/eu/magkari/mc/tool_switcher/mixin/MixinClientPlayerInteractionManager.java
@@ -1,5 +1,6 @@
 package eu.magkari.mc.tool_switcher.mixin;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
@@ -9,17 +10,27 @@ import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Map;
+import java.util.Objects;
 
 import static eu.magkari.mc.tool_switcher.ToolSwitcher.toggled;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class MixinClientPlayerInteractionManager {
+	@Unique
+	private static final Map<TagKey<Block>, TagKey<Item>> TOOL_MAP = Map.of(
+			BlockTags.PICKAXE_MINEABLE, ItemTags.PICKAXES,
+			BlockTags.AXE_MINEABLE, ItemTags.AXES,
+			BlockTags.SHOVEL_MINEABLE, ItemTags.SHOVELS,
+			BlockTags.HOE_MINEABLE, ItemTags.HOES
+	);
 
 	@Inject(method = "attackBlock", at = @At("HEAD"))
 	private void onAttackBlock(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
@@ -29,43 +40,33 @@ public class MixinClientPlayerInteractionManager {
 
 			BlockState state = client.world.getBlockState(pos);
 
-			TagKey<Item> toolTag = null;
-			if (state.isIn(BlockTags.PICKAXE_MINEABLE)) {
-				toolTag = ItemTags.PICKAXES;
-			} else if (state.isIn(BlockTags.AXE_MINEABLE)) {
-				toolTag = ItemTags.AXES;
-			} else if (state.isIn(BlockTags.SHOVEL_MINEABLE)) {
-				toolTag = ItemTags.SHOVELS;
-			} else if (state.isIn(BlockTags.HOE_MINEABLE)) {
-				toolTag = ItemTags.HOES;
-			}
-			if (toolTag != null) {
-				switchTool(toolTag, state);
-			}
+			TOOL_MAP.entrySet().stream()
+					.filter(entry -> state.isIn(entry.getKey()))
+					.map(Map.Entry::getValue)
+					.findFirst().ifPresent(toolTag -> switchTool(toolTag, state, client, pos));
 		}
 	}
 
 	@Unique
-	private void switchTool(TagKey<Item> toolTag, BlockState state) {
-		MinecraftClient client = MinecraftClient.getInstance();
-		if (client.player == null && !client.player.getAbilities().creativeMode) return;
+	private void switchTool(TagKey<Item> toolTag, BlockState state, MinecraftClient client, BlockPos pos) {
+		if (client.player == null || Objects.requireNonNull(client.interactionManager).getCurrentGameMode() != GameMode.SURVIVAL)
+			return;
+		var inventory = client.player.getInventory();
 
 		int bestSlot = -1;
-		int bestEfficiency = -1;
+		float bestEfficiency = -1;
 
+		int oldSlot = inventory.getSelectedSlot();
 		for (int i = 0; i < 9; i++) {
-			if (client.player.getInventory().getStack(i).isIn(toolTag)) {
-				int efficiency = (int) client.player.getInventory().getStack(i).getMiningSpeedMultiplier(state);
-				if (efficiency > bestEfficiency) {
-					bestEfficiency = efficiency;
-					bestSlot = i;
-				}
+			var stack = inventory.getStack(i);
+			if (!stack.isIn(toolTag)) continue;
+			inventory.setSelectedSlot(i);
+			var efficiency = stack.getMiningSpeedMultiplier(state);
+			if (efficiency > bestEfficiency) {
+				bestEfficiency = efficiency;
+				bestSlot = i;
 			}
 		}
-
-		if (bestSlot != -1) {
-			client.player.getInventory().setSelectedSlot(bestSlot);
-		}
+		inventory.setSelectedSlot(bestSlot);
 	}
 }
-
